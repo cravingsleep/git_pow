041839568a92020b9d5bc26314e41674e2dbdabd
Nick Barker <cravingsleep@gmail.com>
2017-08-20 13:34:07 +0100

    Added in getopts for committing script..

--- pow_commit.sh
+++ pow_commit.sh
@@ -1,7 +1,43 @@
-commit_message=$1
-# How many zeros at the start of the commit hash?
-zeros_at_start=$2
-zero_regex_string=$(printf "%0${zeros_at_start}d" 0)
+#!/usr/bin/env bash
+# Options must include -m <commit message> and -z <zeroes in pattern to find>
+# Optionals include -n which is whether to use nonce to find hash or not.
+
+use_nonce=false
+
+while getopts ":m:z:n" opt; do
+	case $opt in
+		m)
+			commit_message=$OPTARG
+			;;
+		z)
+			zeroes=$OPTARG
+			;;
+		n)
+			use_nonce=true
+			;;
+		*)
+			echo "Usage pow_commit.sh -m <commit_message> -z <zeroes> -n"
+			exit 1
+			;;
+		\?)
+			echo "Unknown option $OPTARG"
+			exit 1
+			;;
+		:)
+			echo "Missing option argument for $OPTARG"
+			exit 1
+			;;
+	esac
+done
+
+
+if [ ! $"commit_message" ] || [ ! "$zeroes" ] || [ ! $"use_nonce" ]
+then
+	echo "Missing options."
+	exit 1
+fi
+
+zero_regex_string=$(printf "%0${zeroes}d" 0)
 
 added_files=$(git add .)
 
@@ -11,14 +47,29 @@ then
 	exit 1
 fi
 
-git commit --quiet -m "${commit_message}. Nonce: 1."
-commit_hash=$(git rev-parse HEAD)
-
 tries=1
+
+if [ $use_nonce = true ]
+then
+	git commit --quiet -m "${commit_message}. Nonce: 1."
+else
+	git commit --quiet -m "${commit_message}."
+fi
+
+function commit {
+	if [ $use_nonce = true ] 
+	then
+		git commit --amend --quiet -m "${commit_message}. Nonce: ${tries}."
+	else
+		git commit --amend --quiet -m "${commit_message}."
+	fi
+}
+
+commit_hash=$(git rev-parse HEAD)
 
 while [[ $commit_hash != ${zero_regex_string}* ]] 
 do
 tries=$(($tries+1))
-git commit --amend --quiet -m "${commit_message}. Nonce: ${tries}."
+commit
 commit_hash=$(git rev-parse HEAD)
 done
